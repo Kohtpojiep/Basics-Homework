2a7a83de19eefad8397f97728a70875f80e9f945
NotKohtpojiep <57583784+NotKohtpojiep@users.noreply.github.com>
2021-02-09 12:57:00 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,439 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Game
+{
+    /*
+
+    ДЗ: Реализовать консольную игру на основе логической задачки.
+
+    ДЗ: Реализовать наследование, инкапсуляцию и полиморфизм.
+
+    Крестьянину нужно перевезти через реку волка, козу и капусту. 
+    Но лодка такова, что в ней может поместиться только крестьянин, а с ним или один волк, или одна коза, или одна капуста. 
+    Но если оставить волка с козой, то волк съест козу, а если оставить козу с капустой, то коза съест капусту. 
+    Как перевез свой груз крестьянин? 
+
+    1) Перевозим козу
+    2) Перевозим капусту
+    3) Возвращаем козу
+    4) Перевозим волка
+    5) возвращаемся за козой
+    - всех перевезли.
+
+    ----
+
+    Pac
+    + PK(ID)
+    + Title
+    + FK(AuthorID) ref Authors.Id
+
+    Write
+    + ID
+    + Pac
+    + Doc
+
+    Doc
+    + PK(ID)
+    + Text
+    + FK(AuthorID) ref Authors.Id
+
+    ----
+    Blogs
+    + PK(ID)
+    + Title
+    + FK(AuthorID) ref Authors.Id
+
+    Posts
+    + PK(ID)
+    + Text
+    + FK(AuthorID) ref Authors.Id
+
+    Authors
+    + PK(ID)
+    + Name
+
+    
+    
+    */
+
+    // Предел - консоль -.-
+    public class Game
+    {
+        private enum Places
+        {
+            LeftSide,
+            RightSide
+        }
+
+        private enum GameState
+        {
+            NotStarted,
+            Started,
+            Finished,
+            Losted
+        }
+
+        private Places _humanStandsOn = Places.LeftSide;
+        private GameState _gameState = GameState.NotStarted;
+
+        private GameObject[] GameObjects { get; set; }
+        private List<GameObject> GameObjectsOnLeftCoast { get; set; }
+        private List<GameObject> GameObjectsOnRightCoast { get; set; }
+
+        /// <summary>
+        /// Старт игры, проверка компонентов игры
+        /// </summary>
+        public void StartTheGame()
+        {
+            if (_gameState == GameState.Started)
+                throw new Exception("Игра уже была начата");
+
+            if (GameObjects == null)
+                throw new Exception("Инициализируйте объекты");
+
+            GameObjectsOnLeftCoast = GameObjects.ToList();
+            GameObjectsOnRightCoast = new List<GameObject>();
+            _gameState = GameState.Started;
+
+            string resultOfOperation = "Select the move object";
+            while (_gameState != GameState.Finished && _gameState != GameState.Losted)
+            {
+                Console.Clear();
+                Console.WriteLine(resultOfOperation);
+                Console.WriteLine(DisplayInfo());
+                Console.WriteLine("Input here name of move object or just press enter to change coast");
+                resultOfOperation = new string('-', 20) + "\n" +
+                                    ChoiceGameObjectForMoving(Console.ReadLine());
+            }
+        }
+
+        /// <summary>
+        /// Завершение игры
+        /// </summary>
+        public void BreakTheGame()
+        {
+            Console.Clear();
+            if (_gameState == GameState.Losted)
+                Console.WriteLine("You have losted");
+            else
+                Console.WriteLine("You have winned!");
+
+            //GameObjectsOnLeftCoast = GameObjects.ToList();
+            //GameObjectsOnRightCoast = new List<GameObject>();
+            //_gameState = GameState.NotStarted;
+            //_humanStandsOn = Places.LeftSide;
+        }
+
+
+        /// <summary>
+        /// Загрузка объектов этой игры по умолчанию.
+        /// </summary>
+        public void LoadDefaultGameObjects()
+        {
+            Cabbage cabbage = new Cabbage();
+            Goat goat = new Goat();
+            goat.WriteGameObjectThatCanEat(cabbage);
+            Wolf wolf = new Wolf();
+            wolf.WriteGameObjectThatCanEat(goat);
+
+            GameObjects = new GameObject[] { cabbage, goat, wolf };
+        }
+
+        /// <summary>
+        /// Загрузка игровых объектов, если игра не была начата
+        /// Не понятно, что делать, если загружаются несколько объектов одного и того же класса...
+        /// </summary>
+        /// <param name="gameObjects"></param>
+        public void LoadGameObjects(GameObject[] gameObjects)
+        {
+            if (_gameState != GameState.Started)
+            {
+                if (gameObjects == null || gameObjects.Length == 0)
+                    throw new Exception("Пустой массив");
+
+                if (gameObjects.Count(x => IfCorrectGameObjectGetHisTypeName(x) == null) > 0)
+                    throw new Exception("Имеются непредусмотренные классы");
+
+                var max = gameObjects.Max(x =>
+                    gameObjects.Count(y =>
+                        IfCorrectGameObjectGetHisTypeName(y) == IfCorrectGameObjectGetHisTypeName(x))
+                    );
+
+                if (max > 1)
+                    throw new Exception("Имеются повторяющиеся типы");
+
+                GameObjects = gameObjects;
+            }
+            else
+            {
+                throw new Exception("Игра уже идет...");
+            }
+        }
+
+        private string IfCorrectGameObjectGetHisTypeName(GameObject gameObject)
+        {
+            if (gameObject is Wolf)
+                return typeof(Wolf).Name;
+            if (gameObject is Cabbage)
+                return typeof(Cabbage).Name;
+            if (gameObject is Goat)
+                return typeof(Goat).Name;
+            return null;
+        }
+
+        /// <summary>
+        /// Отображение информации об игре
+        /// </summary>
+        /// <returns></returns>
+        private string DisplayInfo()
+        {
+            if (GameObjectsOnLeftCoast == null && GameObjectsOnRightCoast == null)
+            {
+                return "Nothing to show there";
+            }
+
+            string mapInformation = String.Join('\n', new[]
+            {
+                new string('-', 20),
+                "Left coast:",
+                GetTypeNamesOfGameObjectsArray(GameObjectsOnLeftCoast.ToArray()),
+                "\n",
+                "Right coast:",
+                GetTypeNamesOfGameObjectsArray(GameObjectsOnRightCoast.ToArray()),
+                "\n",
+                $"The human stands on: {GetCurrentPlace()}",
+                new string('-', 20)
+            });
+            return mapInformation;
+        }
+
+        private string GetCurrentPlace()
+        {
+            return _humanStandsOn switch
+            {
+                Places.LeftSide => "Left side",
+                Places.RightSide => "Right side",
+                _ => "Error"
+            };
+        }
+
+        /// <summary>
+        /// Вывод названия всех игровых объектов, находящихся в массиве
+        /// </summary>
+        /// <param name="gameObjects"></param>
+        /// <returns></returns>
+        private string GetTypeNamesOfGameObjectsArray(GameObject[] gameObjects)
+        {
+            if (gameObjects == null)
+                return null;
+
+            if (gameObjects.Length > 0)
+            {
+                List<string> result = new List<string>();
+                foreach (var gameObject in gameObjects)
+                {
+                    result.Add(gameObject.GetType().Name);
+                }
+                return string.Join(',', result);
+            }
+
+            return null;
+        }
+
+
+        /// <summary>
+        /// Проверка поведения объектов на противоположном берегу.
+        /// </summary>
+        private void CheckOppositeCoast()
+        {
+            switch (_humanStandsOn)
+            {
+                case Places.LeftSide:
+                    foreach (var gameObject in GameObjectsOnRightCoast)
+                    {
+                        if (gameObject.CanItEatOneOfThem(GameObjectsOnRightCoast.ToArray()) == true)
+                        {
+                            _gameState = GameState.Losted;
+                            BreakTheGame();
+                        }
+                    }
+                    break;
+                case Places.RightSide:
+                    foreach (var gameObject in GameObjectsOnLeftCoast)
+                    {
+                        if (gameObject.CanItEatOneOfThem(GameObjectsOnLeftCoast.ToArray()) == true)
+                        {
+                            _gameState = GameState.Losted;
+                            BreakTheGame();
+                        }
+                    }
+                    break;
+            }
+
+            if (GameObjectsOnLeftCoast.Count == 0)
+            {
+                _gameState = GameState.Finished;
+                BreakTheGame();
+            }
+        }
+
+        /// <summary>
+        /// Выбор игрового объекта для перемещения по его наименованию.
+        /// </summary>
+        /// <param name="gameObjectTypeName"></param>
+        /// <returns></returns>
+        private string ChoiceGameObjectForMoving(string gameObjectTypeName)
+        {
+            if (String.IsNullOrEmpty(gameObjectTypeName))
+            {
+                MoveToOtherPlace();
+                CheckOppositeCoast();
+                return "Changed place";
+            }
+            foreach (var gameObject in GameObjects)
+            {
+                if (gameObject.GetType().Name.ToLower() == gameObjectTypeName.ToLower())
+                {
+                    string moveResult = MoveToOtherPlace(gameObject);
+                    if (moveResult != null)
+                        return moveResult;
+
+                    CheckOppositeCoast();
+                    return $"Changed place and moved {gameObjectTypeName}";
+                }
+            }
+
+            return "That name of object was not found";
+        }
+
+        /// <summary>
+        /// Перемещение на другое место. Перемещение объекта опционально. 
+        /// </summary>
+        /// <param name="gameObject"></param>
+        /// <returns></returns>
+        private string MoveToOtherPlace(GameObject gameObject = null)
+        {
+            switch (_humanStandsOn)
+            {
+                case Places.LeftSide:
+                    if (gameObject != null)
+                    {
+                        if (GameObjectsOnLeftCoast
+                            .Exists(x => x.GetType().Name == gameObject.GetType().Name) == false)
+                            return "This object was not found on left coast";
+
+                        GameObjectsOnLeftCoast.Remove(gameObject);
+                        GameObjectsOnRightCoast.Add(gameObject);
+                    }
+                    _humanStandsOn = Places.RightSide;
+                    break;
+
+                case Places.RightSide:
+                    if (gameObject != null)
+                    {
+                        if (GameObjectsOnRightCoast
+                            .Exists(x => x.GetType().Name == gameObject.GetType().Name) == false)
+                            return "This object was not found on right coast";
+
+                        GameObjectsOnRightCoast.Remove(gameObject);
+                        GameObjectsOnLeftCoast.Add(gameObject);
+                    }
+                    _humanStandsOn = Places.LeftSide;
+                    break;
+
+                default:
+                    throw new Exception("Человек находится на непредусмотренном месте");
+            }
+
+            return null;
+        }
+    }
+
+    public abstract class GameObject
+    {
+        private GameObject[] WhatCanEat { get; set; }
+
+        private bool CanIBeEatenByMyself(GameObject[] gameObjects)
+        {
+            foreach (var gameObject in gameObjects)
+            {
+                if (gameObject.GetType() == this.GetType())
+                    return true;
+            }
+
+            return false;
+        }
+        public void WriteGameObjectThatCanEat(GameObject gameObject)
+        {
+            GameObject[] gameObjects = new[] { gameObject };
+
+            if (CanIBeEatenByMyself(gameObjects) == true)
+                throw new Exception("Объект не может съесть самого себя");
+
+            WhatCanEat = gameObjects;
+        }
+        public void WriteGameObjectsThatCanEat(GameObject[] gameObjects)
+        {
+            WhatCanEat = gameObjects;
+        }
+        public bool CanItEatOneOfThem(GameObject[] gameObjects)
+        {
+            if (WhatCanEat == null || WhatCanEat.Length == 0)
+                return false;
+
+            foreach (var canEatGameObject in WhatCanEat)
+            {
+                foreach (var gameObject in gameObjects)
+                {
+                    if (canEatGameObject.GetType().Name == gameObject.GetType().Name)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    public class Wolf : GameObject
+    {
+    }
+
+    public class Goat : GameObject
+    {
+    }
+
+    public class Cabbage : GameObject
+    {
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            // Список левого берега
+            // Список правого берега
+            // Добавляем в левый берег объекты
+            // Старт игры
+            // Выбираем объект для перевозки
+            // Помещаем его на лодку
+            // Сравниваем все объекты на берегах
+            // Если нет проблем, то перемещаем
+            // Иначе игра завершается
+
+            Cabbage cabbage = new Cabbage();
+
+            Goat goat = new Goat();
+            goat.WriteGameObjectThatCanEat(cabbage);
+
+            Wolf wolf = new Wolf();
+            wolf.WriteGameObjectThatCanEat(goat);
+
+            GameObject[] gameObjects = { cabbage, goat, wolf };
+            Game game = new Game();
+            game.LoadGameObjects(gameObjects);
+            game.StartTheGame();
+        }
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP
+{
+    class Program
+    {
+        /*
+         * 1) Инкапсуляция - это вроде некой компановки элементов по своим ящикам.
+         * Когда, например, в коробке из под игрушек не должно оказаться носка или тетради,
+         * а они должны были находиться на своих местах (носки в коробке с вещами, а тетрадь
+         * в коробке с макулатурой)
+         * 2) Инкапсуляция - это когда мы предоставляем некий набор возможностей пользователю,
+         * не раскрывая ему деталей о том, как это работает
+         * (Например, зачем вам нужно знать о том, как проходят механизмы пробегания тока у пульта и
+         * как телевизор будет обрабатывать испускаемый инфокрасный луч, когда вы нажали на одну из кнопок
+         * Вам только нужно знать, какой результат вернет эта кнопка, если вы на нее просто нажмете)
+        */
+        // Что не является икапсуляцией
+        private class Pen
+        {
+            private string _socksName = "Дружбан";
+            public void RepairMachine() { }
+        }
+
+        // Пример инкапсуляции
+        private class RemoteControllerButton
+        {
+            private string Name { get; set; }
+            private int Number { get; set; }
+            public RemoteControllerButton(string name, int number)
+            {
+                Name = name;
+                Number = number;
+            }
+            public void Press() { }
+            public int GetNumber()
+            {
+                return Number;
+            }
+        }
+        private class RemoteController
+        {
+            private RemoteControllerButton[] RemoteControllerButtons { get; set; }
+            private string Model
+            {
+                set
+                {
+                    if (value != null)
+                        Model = value;
+                }
+                get { return Model; }
+            }
+            public RemoteController(RemoteControllerButton[] buttons, string model)
+            {
+                RemoteControllerButtons = buttons;
+                Model = model;
+            }
+            public int GetButtonsCount()
+            {
+                return RemoteControllerButtons.Length;
+            }
+            public void PressOnButton(int buttonNumber)
+            {
+                foreach (var remoteControllerButton in RemoteControllerButtons)
+                {
+                    if (remoteControllerButton.GetNumber() == buttonNumber)
+                        remoteControllerButton.Press();
+                }
+            }
+        }
+
+
+        /*
+         * Наследование - это когда дочерний класс имеет доступ к свойствам и функционалу своих потомков.
+         * Обычно наследование используется для дополнения своих потомков, путем добавления в дочерний
+         * класс некого функционала.
+        */
+
+        // Что не является наследованием
+        private class Animal
+        {
+            private int _age;
+            private string Name { get; set; }
+            public Animal(int age)
+            {
+                _age = age;
+            }
+            public void Voice() { }
+        }
+        private class DocherniiAnimal
+        {
+            private int _age;
+            private int _weight;
+            private string Name { get; set; }
+            public DocherniiAnimal(int age, int weight)
+            {
+                _age = age;
+                _weight = weight;
+            }
+            public void Voice() { }
+            public void Walk() { }
+        }
+
+        // Пример наследования
+        private class Subject
+        {
+            protected int Weight { get; private set; }
+            public void SetWeight(int weight)
+            {
+                Weight = weight;
+            }
+            public void Destroy() { }
+        }
+
+        private class Paper : Subject
+        {
+            public Paper(int weight)
+            {
+                SetWeight(weight);
+            }
+
+            public int GetWeight()
+            {
+                return Weight;
+            }
+        }
+
+        /*
+         * Полиморфизм - это когда одно и то же действие базового класса (может) выполняться по разному у
+         * наследуемых ему классах.
+         * Для работы полиморфизма требуется наследование.
+        */
+
+        // Пример полиморфизма
+        private class BaseDoings
+        {
+            public virtual void Open() { }
+            public virtual void Close() { }
+        }
+
+        private class MSSQL : BaseDoings
+        {
+            public override void Open()
+            {
+                // Provide connection to MSSQL Server
+                Console.WriteLine("Connected to MSSQL Server");
+            }
+
+            public override void Close()
+            {
+                // Closing connection to MSSQL Server
+                Console.WriteLine("Disconnected from MSSQL Server");
+            }
+        }
+
+        private class MySQL : BaseDoings
+        {
+            public override void Open()
+            {
+                // Provide connection to MSSQL Server
+                Console.WriteLine("Connected to MySQL Server");
+            }
+
+            public override void Close()
+            {
+                // Closing connection to MSSQL Server
+                Console.WriteLine("Disconnected from MySQL Server");
+            }
+        }
+
+        // Классы, которые докажут, что использовать полиморфизм круто
+        private class NotCoolBaseDoings
+        {
+            public void Open()
+            {
+                // Just trolling if not overrided...
+                Console.WriteLine("ConnectionDone");
+            }
+        }
+        private class NotCoolMSSQL : NotCoolBaseDoings
+        {
+            public void Open()
+            {
+                // Provide connection to MSSQL Server
+                Console.WriteLine("Connected to MSSQL Server");
+            }
+        }
+
+        private class NotCoolMySQL : NotCoolBaseDoings
+        {
+            public void Open()
+            {
+                // Provide connection to MSSQL Server
+                Console.WriteLine("Connected to MySQL Server");
+            }
+        }
+
+        static void Main()
+        {
+            // Пример наследования
+            Subject subject = new Subject();
+            subject.SetWeight(10);
+            subject.Destroy();
+            Paper paper = new Paper(15);
+            paper.Destroy();
+            paper.GetWeight();
+
+            Subject ivisedPaper = paper;
+            //ivisedPaper.GetWeight();   // Не сработает, хотя это бумага
+            //Paper newPaper = new Subject(); // В обратную сторону наследование не работает
+
+            // Пример полиморфизма
+            Console.WriteLine("Полиморфизм:");
+            BaseDoings[] connections = { new MSSQL(), new MySQL() };
+            for(int i = 0; i < connections.Length; i++)
+                connections[i].Open();
+
+            Console.WriteLine("Наследование без использования полиморфизма:");
+            NotCoolBaseDoings[] notCoolConnections = { new NotCoolMSSQL(), new NotCoolMySQL() };
+            for(int i = 0; i < notCoolConnections.Length; i++)
+                notCoolConnections[i].Open();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
